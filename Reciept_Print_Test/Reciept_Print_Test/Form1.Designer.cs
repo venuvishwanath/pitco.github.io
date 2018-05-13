1108ae3cd698e005aa15634dc16dc7c5333097c2
Venu <chinthavenu20@gmail.com>
2018-05-13 21:44:06 +0530

    receipt project
    added new files

---
+++ Reciept_Print_Test/Reciept_Print_Test/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Reciept_Print_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddItem = new System.Windows.Forms.Button();
+            this.btnRemoveItem = new System.Windows.Forms.Button();
+            this.btnPrintReciept = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(31, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 329);
+            this.listBox1.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(322, 60);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(196, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(354, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Equipment ID";
+            // 
+            // btnAddItem
+            // 
+            this.btnAddItem.Location = new System.Drawing.Point(322, 113);
+            this.btnAddItem.Name = "btnAddItem";
+            this.btnAddItem.Size = new System.Drawing.Size(196, 55);
+            this.btnAddItem.TabIndex = 6;
+            this.btnAddItem.Text = "Add Item";
+            this.btnAddItem.UseVisualStyleBackColor = true;
+            this.btnAddItem.Click += new System.EventHandler(this.btnAddItem_Click_1);
+            // 
+            // btnRemoveItem
+            // 
+            this.btnRemoveItem.Location = new System.Drawing.Point(322, 196);
+            this.btnRemoveItem.Name = "btnRemoveItem";
+            this.btnRemoveItem.Size = new System.Drawing.Size(196, 55);
+            this.btnRemoveItem.TabIndex = 7;
+            this.btnRemoveItem.Text = "Remove Item";
+            this.btnRemoveItem.UseVisualStyleBackColor = true;
+            this.btnRemoveItem.Click += new System.EventHandler(this.btnRemoveItem_Click);
+            // 
+            // btnPrintReciept
+            // 
+            this.btnPrintReciept.Location = new System.Drawing.Point(326, 298);
+            this.btnPrintReciept.Name = "btnPrintReciept";
+            this.btnPrintReciept.Size = new System.Drawing.Size(192, 55);
+            this.btnPrintReciept.TabIndex = 8;
+            this.btnPrintReciept.Text = "Print Receipt";
+            this.btnPrintReciept.UseVisualStyleBackColor = true;
+            this.btnPrintReciept.Click += new System.EventHandler(this.btnPrintReciept_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 413);
+            this.Controls.Add(this.btnPrintReciept);
+            this.Controls.Add(this.btnRemoveItem);
+            this.Controls.Add(this.btnAddItem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddItem;
+        private System.Windows.Forms.Button btnRemoveItem;
+        private System.Windows.Forms.Button btnPrintReciept;
+    }
+}
+
---
+++ Reciept_Print_Test/Reciept_Print_Test/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+using Microsoft.VisualBasic;
+
+namespace Reciept_Print_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtName.Focus();
+            label1.Font = new Font("Arial", 16);
+        }
+        
+
+        private void btnAddItem_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(txtName.Text.PadRight(30)); // + txtPrice.Text);
+            txtName.Text = "";
+           //txtPrice.Text = "";
+        }
+        //private void btnAddItem_Click_1(object sender, EventArgs e)
+        //{
+        //    listBox1.Items.Add(txtName.Text.PadRight(30) + txtPrice.Text);
+        //    txtName.Text = "";
+        //    txtPrice.Text = "";
+        //}
+        private void btnRemoveItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            catch (Exception)
+            {
+
+                return;
+            }
+        }
+
+        private void btnPrintReciept_Click_1(object sender, EventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+
+            PrintDocument printDocument = new PrintDocument();
+
+            printDialog.Document = printDocument; //add the document to the dialog box...        
+
+            printDocument.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(CreateReceipt); //add an event handler that will do the printing
+
+            //on a till you will not want to ask the user where to print but this is fine for the test envoironment.
+
+            DialogResult result = printDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                printDocument.Print();
+
+            }
+        }
+
+        public void CreateReceipt(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+            
+            float cash = 10;
+            float change = 0.00f;
+
+            //this prints the reciept
+
+            Graphics graphic = e.Graphics;
+
+            Font font = new Font("Courier New", 14); //must use a mono spaced font as the spaces need to line up
+
+            float fontHeight = font.GetHeight();
+
+            int startX = 10;
+            int startY = 10;
+            int offset = 40;
+
+            graphic.DrawString("COMMUNICATIONS CENTRAL STORES", new Font("Courier New", 20), new SolidBrush(Color.Black), startX, startY);
+            string top = "Equipment ID".PadLeft(20);// +"Price";
+            graphic.DrawString(top, font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + (int)fontHeight; //make the spacing consistent
+            graphic.DrawString("================================", font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + (int)fontHeight + 5; //make the spacing consistent
+
+            string totalprice = listBox1.Items.Count.ToString();
+
+            foreach (string item in listBox1.Items)
+            {
+                //create the string to print on the reciept
+                string productDescription = item;
+                //string productTotal = item.Substring(item.Length - 6, 6);
+                //float productPrice = float.Parse(item.Substring(item.Length - 5, 5));
+
+              //  MessageBox.Show(item.Substring(item.Length - 5, 5) );//+ "PROD TOTAL: " + productTotal);
+
+
+                //totalprice += productPrice;
+
+                if (productDescription.Contains("  -"))
+                {
+                    string productLine = productDescription.Substring(0, 24);
+
+                    graphic.DrawString(productLine, new Font("Courier New", 14, FontStyle.Italic), new SolidBrush(Color.Red), startX, startY + offset);
+
+                    offset = offset + (int)fontHeight + 5; //make the spacing consistent
+                }
+                else
+                {
+                    string productLine = productDescription;
+
+                    graphic.DrawString(productLine, font, new SolidBrush(Color.Black), startX, startY + offset);
+
+                    offset = offset + (int)fontHeight + 5; //make the spacing consistent
+                }
+
+            }
+
+           
+            //when we have drawn all of the items add the total
+
+            offset = offset + 20; //make some room so that the total stands out.
+
+            graphic.DrawString("-------------------------------", font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + (int)fontHeight + 5; //make the spacing consistent
+
+            graphic.DrawString("Total Number of Items   =".PadRight(30) + String.Format("{0:c}", totalprice), new Font("Courier New", 14, FontStyle.Bold), new SolidBrush(Color.Black), startX, startY + offset);
+
+            offset = offset + 30; //make some room so that the total stands out.
+            //graphic.DrawString("CASH ".PadRight(30) + String.Format("{0:c}", cash), font, new SolidBrush(Color.Black), startX, startY + offset);
+            //offset = offset + 15;
+            //graphic.DrawString("CHANGE ".PadRight(30) + String.Format("{0:c}", change), font, new SolidBrush(Color.Black), startX, startY + offset);
+            //offset = offset + 30; //make some room so that the total stands out.
+            graphic.DrawString("     Thank-you ", font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + 15;
+           
+
+        }
+
+      
+        
+
+       
+
+       
+
+        
+    }
+}
