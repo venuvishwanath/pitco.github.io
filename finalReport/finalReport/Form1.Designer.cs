1a62e1d579fd16527a56e9f015d4692d2e3a784c
venuvishwanath <38915054+venuvishwanath@users.noreply.github.com>
2018-05-20 11:34:44 +0530

    new project using crystal reports

---
+++ finalReport/finalReport/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace finalReport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label3;
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.Location = new System.Drawing.Point(97, 27);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(149, 19);
+            label1.TabIndex = 5;
+            label1.Text = "SELECT DISTRICT";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label3.Location = new System.Drawing.Point(97, 71);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(160, 19);
+            label3.TabIndex = 7;
+            label3.Text = "ENTER SET MODEL";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(90, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(803, 194);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(541, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 71);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "PRINT RECEIPT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(86, 412);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(803, 217);
+            this.crystalReportViewer1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "HYDERABAD",
+            "RANGAREDDY",
+            "WARANGAL",
+            "ADILABAD",
+            "KARIMNAGAR"});
+            this.comboBox1.Location = new System.Drawing.Point(267, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "- - - -select dist- - - -";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(267, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(225, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 71);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "DELETE ROW";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 641);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(label3);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ finalReport/finalReport/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace finalReport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(175,238,238);
+            //create table with three columns
+
+            DataTable t = new DataTable();
+            
+            t.Columns.Add("S.NO", typeof(Int16));
+            t.Columns.Add("SET NO", typeof(string));
+            t.Columns.Add("Rx SENSITIVITY", typeof(string));
+            t.Columns.Add("Tx OUTPUT", typeof(string));
+            t.Columns.Add("MODULATION", typeof(string));
+            t.Columns.Add("REMARKS", typeof(string));
+
+           
+
+            //bind table to datagridview
+            dataGridView1.DataSource = t;
+
+
+
+            //DataSet dsReport = new dsCDCatalog();
+            //// create temp dataset to read xml information 
+            //DataSet dsTempReport = new DataSet();
+            
+                
+
+                
+
+            
+              
+            
+        }
+
+        //public DataSet CustomMethod()
+        //{
+        //    //Define a dataset
+
+        //    DataSet ds = new DataSet();
+
+        //    //Define a datatable
+
+        //    DataTable dt = new DataTable();
+        //    dt.Columns.Add("S.NO", typeof(Int16));
+        //    dt.Columns.Add("SET NO", typeof(string));
+        //    dt.Columns.Add("Rx SENSITIVITY", typeof(string));
+        //    dt.Columns.Add("Tx OUTPUT", typeof(string));
+        //    dt.Columns.Add("MODULATION", typeof(string));
+        //    dt.Columns.Add("REMARKS", typeof(string));
+        //   // dt.Columns.Add("equipment_name", typeof(string));
+        //    //dt.Columns.Add("district_name", typeof(string));
+
+        //    //Write DataGridView data to the datatable
+        //    foreach (DataGridViewRow dgv in dataGridView1.Rows)
+        //    {
+        //        dt.Rows.Add(dgv.Cells[0].Value, dgv.Cells[1].Value, dgv.Cells[2].Value, dgv.Cells[3].Value, dgv.Cells[4].Value, dgv.Cells[5].Value);
+        //    }
+
+        //   // dt.Rows.Add(textBox1.Text,comboBox1.SelectedItem);
+        //    //Add datatable to the dataset and convert to an XML format file
+
+        //    ds.Tables.Add(dt);
+        //    ds.WriteXmlSchema("data.xml");
+
+        //    return ds;
+        //}
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //Define a dataset
+
+            DataSet ds = new DataSet();
+
+            //Define a datatable
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("S.NO", typeof(Int16));
+            dt.Columns.Add("SET NO", typeof(string));
+            dt.Columns.Add("Rx SENSITIVITY", typeof(string));
+            dt.Columns.Add("Tx OUTPUT", typeof(string));
+            dt.Columns.Add("MODULATION", typeof(string));
+            dt.Columns.Add("REMARKS", typeof(string));
+            dt.Columns.Add("ID");
+
+            //Write DataGridView data to the datatable
+            foreach (DataGridViewRow dgv in dataGridView1.Rows)
+            {
+                dt.Rows.Add(dgv.Cells[0].Value, dgv.Cells[1].Value, dgv.Cells[2].Value,dgv.Cells[3].Value,dgv.Cells[4].Value,dgv.Cells[5].Value);
+            }
+
+
+            //Add datatable to the dataset and convert to an XML format file
+
+            ds.Tables.Add(dt);
+           
+
+
+
+
+            //creating second table
+
+            DataTable dt2 = new DataTable();
+            dt2.Columns.Add("model");
+            dt2.Columns.Add("district");
+            dt2.Columns.Add("ID");
+            dt2.Rows.Add(textBox1.Text);
+            dt2.Rows.Add(comboBox1.SelectedText);
+
+            ds.Tables.Add(dt2);
+            ds.WriteXmlSchema("data.xml");
+            
+
+            //transefer data to crystalreportviewer
+            
+            
+            CrystalReport1 cr = new CrystalReport1();
+            cr.SetDataSource(ds.Tables[0]);
+            crystalReportViewer1.ReportSource = cr;
+            
+
+            //Form2 f2 = new Form2();
+            //f2.ShowDialog();
+            
+        }
+    }
+}
